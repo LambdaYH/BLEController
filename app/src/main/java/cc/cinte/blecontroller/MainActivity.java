a74fb4f7a010401bc1218d07000c1f151cf6d62c
Lambda_YH <chenyihang1998@gmail.com>
2020-04-22 22:23:25 +0800

    s

--- app/src/main/java/cc/cinte/blecontroller/MainActivity.java
+++ app/src/main/java/cc/cinte/blecontroller/MainActivity.java
@@ -5,13 +5,17 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.bluetooth.BluetoothDevice;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import com.inuker.bluetooth.library.BluetoothClient;
@@ -24,6 +28,7 @@ import com.inuker.bluetooth.library.search.SearchRequest;
 import com.inuker.bluetooth.library.search.SearchResult;
 import com.inuker.bluetooth.library.search.response.SearchResponse;
 import com.inuker.bluetooth.library.utils.BluetoothLog;
+import com.inuker.bluetooth.library.utils.BluetoothUtils;
 
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -36,10 +41,27 @@ import java.util.UUID;
 public class MainActivity extends AppCompatActivity { ;
     private static final int MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION = 1;
     ArrayList<String> list = new ArrayList();
+    ArrayList listAds = new ArrayList();
     ArrayAdapter adapter;
     private Spinner deviceslist;
+    private BluetoothDevice mDevice;
     String address;
+    private DeviceDetailAdapter mAdapter;
     public static final int REQUEST_SUCCESS = Code.REQUEST_SUCCESS;
+
+    public ImageView Blestatus;
+
+    private boolean isConnected=false;
+
+    private int bleposition=0;
+
+    private UUID service =null;
+    private UUID character =null;
+
+    private byte[] bytes;
+    public boolean isSwitch;
+    public Switch mSwitch;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -50,6 +72,20 @@ public class MainActivity extends AppCompatActivity { ;
         deviceslist.setAdapter(adapter);
         deviceslist.setOnItemSelectedListener(myItemSelectedListener);
         requestPosition();
+        ImageView Blestatus = findViewById(R.id.BleStatus);
+        mSwitch=findViewById(R.id.switchLight);
+        mSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked){
+                    bytes= new byte[]{'O', 'N','\n'};
+                    sendMsg();
+                }else {
+                    bytes=new byte[]{'O','F','F','\n'};
+                    sendMsg();
+                }
+            }
+        });
     }
     public void scanBLE(View view){
         list.clear();
@@ -69,6 +105,8 @@ public class MainActivity extends AppCompatActivity { ;
                 BluetoothLog.v(String.format("beacon for %s\n%s", device.getAddress(), beacon.toString()));
                 if(device.getName().contains("LED")) {
                     list.add(device.getName() + "\n" + device.getAddress());
+                    listAds.add(device.getAddress());
+                    listAds = new ArrayList<>(new HashSet<>(listAds));
                     list  = new ArrayList<>(new HashSet<>(list));
                     adapter.notifyDataSetChanged();
                 }
@@ -85,8 +123,7 @@ public class MainActivity extends AppCompatActivity { ;
             }
         });
     }
-    public void sendMsg(View v){
-        byte[] bytes={'s','b','h','h','q'};
+    public void sendMsg(){
         UUID serviceUUID= UUID.fromString("0000ffe0-0000-1000-8000-00805f9b34fb");
         UUID characterUUID = UUID.fromString("0000ffe1-0000-1000-8000-00805f9b34fb");
         ClientManager.getClient().write(address, serviceUUID, characterUUID, bytes, new BleWriteResponse() {
@@ -98,27 +135,40 @@ public class MainActivity extends AppCompatActivity { ;
             }
         });
     }
+
     private AdapterView.OnItemSelectedListener myItemSelectedListener = new AdapterView.OnItemSelectedListener() {
         @Override
         public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-            String info = ((TextView) view).getText().toString();
-            address = info.substring(info.length()-17);
-            ClientManager.getClient().connect(address, new BleConnectResponse() {
-                @Override
-                public void onResponse(int code, BleGattProfile profile) {
-                    if (code == REQUEST_SUCCESS) {
-                        list.add("fine");
-                        adapter.notifyDataSetChanged();
-                    }
-                        }
-            });
-        }
+            bleposition=position;
+            if (!isConnected) {
+                return;
+            }
+            mDevice = BluetoothUtils.getRemoteDevice(address);
+            mAdapter = new DeviceDetailAdapter(MyApplication.getInstance(), mDevice);
+            DetailItem item = (DetailItem) mAdapter.getItem(position);
+            if (item.type == DetailItem.TYPE_CHARACTER) {
+                service=item.service;
+                character=item.uuid;
+            }
 
+
+        }
         @Override
         public void onNothingSelected(AdapterView<?> adapterView) {
 
         }
     };
+    public void connect(View view){
+        address = listAds.get(bleposition).toString();
+        ClientManager.getClient().connect(address, new BleConnectResponse() {
+            @Override
+            public void onResponse(int code, BleGattProfile data) {
+                if(code == REQUEST_SUCCESS){
+                    isConnected=true;
+                }
+            }
+        });
+    }
 
     private void requestPosition(){
         if (ContextCompat.checkSelfPermission(this,

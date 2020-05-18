4e456c7f4f5e1d7d601e1acaebcf5110ffccff5d
Lambda_YH <chenyihang1998@gmail.com>
2020-05-18 15:43:13 +0800

    a lot of update

---
+++ app/src/main/java/cc/cinte/blecontroller/Constants.java
@@ -0,0 +1,12 @@
+package cc.cinte.blecontroller;
+
+import com.inuker.bluetooth.library.Code;
+
+public class Constants {
+    public static final int MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION = 1;
+
+    public static final int STATUS_CONNECTED = 0x10;
+    public static final int STATUS_DISCONNECTED = 0x20;
+
+    public static final int REQUEST_SUCCESS = Code.REQUEST_SUCCESS;
+}
--- app/src/main/java/cc/cinte/blecontroller/MainActivity.java
+++ app/src/main/java/cc/cinte/blecontroller/MainActivity.java
@@ -5,22 +5,23 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
-import android.bluetooth.BluetoothDevice;
+import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.ImageView;
-import android.widget.ListView;
+import android.widget.SeekBar;
 import android.widget.Spinner;
 import android.widget.Switch;
-import android.widget.TextView;
+
 
 import com.inuker.bluetooth.library.BluetoothClient;
-import com.inuker.bluetooth.library.Code;
 import com.inuker.bluetooth.library.beacon.Beacon;
+import com.inuker.bluetooth.library.connect.listener.BleConnectStatusListener;
 import com.inuker.bluetooth.library.connect.response.BleConnectResponse;
 import com.inuker.bluetooth.library.connect.response.BleWriteResponse;
 import com.inuker.bluetooth.library.model.BleGattProfile;
@@ -28,46 +29,102 @@ import com.inuker.bluetooth.library.search.SearchRequest;
 import com.inuker.bluetooth.library.search.SearchResult;
 import com.inuker.bluetooth.library.search.response.SearchResponse;
 import com.inuker.bluetooth.library.utils.BluetoothLog;
-import com.inuker.bluetooth.library.utils.BluetoothUtils;
 
 import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Set;
 import java.util.UUID;
 
 
+
+
 public class MainActivity extends AppCompatActivity { ;
-    private static final int MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION = 1;
     ArrayList<String> list = new ArrayList();
     ArrayList listAds = new ArrayList();
     ArrayAdapter adapter;
-    private Spinner deviceslist;
     String address;
-    public static final int REQUEST_SUCCESS = Code.REQUEST_SUCCESS;
 
     public ImageView Blestatus;
+
+    private Button mConnectButton;
+    public SeekBar R_SB;
+    public SeekBar G_SB;
+    public SeekBar B_SB;
 
     private boolean isConnected=false;
 
     private int bleposition=0;
 
-    private UUID service =null;
-    private UUID character =null;
+    private BluetoothClient mClient;
 
     private byte[] bytes;
     public Switch mSwitch;
+    private String R_Value = String.valueOf(255);
+    private String G_Value = String.valueOf(255);
+    private String B_Value = String.valueOf(255);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        deviceslist = findViewById(R.id.BLElist);
+        Spinner devices = findViewById(R.id.BLElist);
+
+        mClient=ClientManager.getClient();
+
+        mConnectButton=findViewById(R.id.isconnect);
+
+        R_SB=findViewById(R.id.R_Pixel);
+        G_SB=findViewById(R.id.G_Pixel);
+        B_SB=findViewById(R.id.B_Pixel);
+        R_SB.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                R_Value= String.valueOf(progress);
+                sendMsg();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+        G_SB.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                G_Value= String.valueOf(progress);
+                sendMsg();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+        B_SB.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                B_Value= String.valueOf(progress);
+                sendMsg();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+
         adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, list);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        deviceslist.setAdapter(adapter);
-        deviceslist.setOnItemSelectedListener(myItemSelectedListener);
+        devices.setAdapter(adapter);
+        devices.setOnItemSelectedListener(myItemSelectedListener);
         requestPosition();
         Blestatus=findViewById(R.id.BleStatus);
         mSwitch=findViewById(R.id.switchLight);
@@ -76,10 +133,10 @@ public class MainActivity extends AppCompatActivity { ;
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (isChecked){
                     bytes= new byte[]{'O', 'N','\n'};
-                    sendMsg();
+                    switchMsg();
                 }else {
                     bytes=new byte[]{'O','F','F','\n'};
-                    sendMsg();
+                    switchMsg();
                 }
             }
         });
@@ -90,7 +147,7 @@ public class MainActivity extends AppCompatActivity { ;
                 .searchBluetoothLeDevice(3000, 1)   // 先扫BLE设备3次，每次3s
                 .build();
 
-        ClientManager.getClient().search(request, new SearchResponse() {
+        mClient.search(request, new SearchResponse() {
             @Override
             public void onSearchStarted() {
 
@@ -123,11 +180,23 @@ public class MainActivity extends AppCompatActivity { ;
     public void sendMsg(){
         UUID serviceUUID= UUID.fromString("0000ffe0-0000-1000-8000-00805f9b34fb");
         UUID characterUUID = UUID.fromString("0000ffe1-0000-1000-8000-00805f9b34fb");
-        ClientManager.getClient().write(address, serviceUUID, characterUUID, bytes, new BleWriteResponse() {
+        String byteTemp=G_Value+"|"+B_Value+"|"+R_Value+"\n";
+        bytes=byteTemp.getBytes();
+        mClient.write(address, serviceUUID, characterUUID, bytes, new BleWriteResponse() {
             @Override
             public void onResponse(int code) {
-                if (code == REQUEST_SUCCESS) {
-
+                if (code == Constants.REQUEST_SUCCESS) {
+                }
+            }
+        });
+    }
+    public void switchMsg(){
+        UUID serviceUUID= UUID.fromString("0000ffe0-0000-1000-8000-00805f9b34fb");
+        UUID characterUUID = UUID.fromString("0000ffe1-0000-1000-8000-00805f9b34fb");
+        mClient.write(address, serviceUUID, characterUUID, bytes, new BleWriteResponse() {
+            @Override
+            public void onResponse(int code) {
+                if (code == Constants.REQUEST_SUCCESS) {
                 }
             }
         });
@@ -146,46 +215,75 @@ public class MainActivity extends AppCompatActivity { ;
 
         }
     };
-    public void connect(View view){
+    public void connect_Button(View v){
+        if(isConnected){
+            disconnect();
+        }
+        if(!isConnected){
+             connect();
+        }
+    }
+    public void connect(){
         if(listAds.isEmpty()){
-            return;
         }else{
             address = listAds.get(bleposition).toString();
-            ClientManager.getClient().connect(address, new BleConnectResponse() {
+            mClient.registerConnectStatusListener(address, mBleConnectStatusListener);  //register bluetooth status callback function
+            mClient.connect(address, new BleConnectResponse() {
+                @SuppressLint("SetTextI18n")
                 @Override
                 public void onResponse(int code, BleGattProfile data) {
-                    if(code == REQUEST_SUCCESS){
-                        isConnected=true;
-                        Blestatus.setImageResource(R.drawable.connected);
+                    if(code == Constants.REQUEST_SUCCESS){
                     }
                 }
             });
         }
     }
-    public void disconnect(View view){
+    public void disconnect(){
         if(listAds.isEmpty()){
-            return;
         }else{
             address = listAds.get(bleposition).toString();
-            ClientManager.getClient().disconnect(address);
-            isConnected=false;
-            Blestatus.setImageResource(R.drawable.disconnected);
+            mClient.disconnect(address);
         }
     }
 
-    public void sendBlue(View v){
-        bytes= new byte[]{'B', 'L','U','E','\n'};
+    public void White_Button(View v){
+        R_Value= String.valueOf(255);
+        G_Value= String.valueOf(255);
+        B_Value= String.valueOf(255);
         sendMsg();
     }
-    public void sendGreen(View v){
-        bytes= new byte[]{'G', 'R','E','E','N','\n'};
+    public void Green_Button(View v){
+        R_Value= String.valueOf(0);
+        G_Value= String.valueOf(255);
+        B_Value= String.valueOf(0);
         sendMsg();
     }
-    public void sendMix(View v){
-        bytes= new byte[]{'M', 'I','X','\n'};
+    public void Blue_Button(View v){
+        R_Value= String.valueOf(0);
+        G_Value= String.valueOf(0);
+        B_Value= String.valueOf(255);
         sendMsg();
     }
 
+    //bluetooth callback function
+    private final BleConnectStatusListener mBleConnectStatusListener = new BleConnectStatusListener() {
+
+        @SuppressLint("SetTextI18n")
+        @Override
+        public void onConnectStatusChanged(String mac, int status) {
+            if (status == Constants.STATUS_CONNECTED) {
+                Blestatus.setImageResource(R.drawable.connected);
+                mConnectButton.setText("Disconnect");
+                isConnected=true;
+            } else if (status == Constants.STATUS_DISCONNECTED) {
+                Blestatus.setImageResource(R.drawable.disconnected);
+                mConnectButton.setText("Connect");
+                isConnected=false;
+            }
+        }
+    };
+
+    //get position permission
     private void requestPosition(){
         if (ContextCompat.checkSelfPermission(this,
                 Manifest.permission.ACCESS_COARSE_LOCATION)
@@ -202,7 +300,7 @@ public class MainActivity extends AppCompatActivity { ;
                 // No explanation needed; request the permission
                 ActivityCompat.requestPermissions(this,
                         new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
-                        MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION);
+                        Constants.MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION);
 
                 // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
                 // app-defined int constant. The callback method gets the
